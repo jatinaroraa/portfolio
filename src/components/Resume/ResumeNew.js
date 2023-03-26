b538e00e01f63a83969b214b82f0c340c481e75f
jatin arora <jatinaroraaaa3@gmail.com>
2023-03-26 16:37:49 +0530

    fix

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/jatinaroraa/portfolio/master/src/Assets/JatinArora2023.pdf";
+  "https://raw.githubusercontent.com/jatinaroraa/portfolio/main/src/Assets/JatinArora2023.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
